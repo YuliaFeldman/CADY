5b679b534307ecb698b60ef786daaa53ae2355a0
Yulia Feldman <48710519+YuliaFeldman@users.noreply.github.com>
2024-09-22 01:29:08 +0300

    putting examples as comments

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ if __name__ == '__main__':
     components_manager = ComponentsManager()
 
     # Examples how to use the system
-    print(components_manager.find_operable_components(5.5, -20))
-    print(components_manager.find_operable_components(0, 0))
-    print(components_manager.find_operable_components(1.8, 10.5))
+    # print(components_manager.find_operable_components(5.5, -20))
+    # print(components_manager.find_operable_components(0, 0))
+    # print(components_manager.find_operable_components(1.8, 10.5))
